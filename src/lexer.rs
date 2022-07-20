85f6aa3caa67f63a13f74a8d591cfce023f3174d
Evryon <fireden70@gmail.com>
2022-07-20 17:04:16 +0200

    Added nom and set up parser function

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,66 +1,65 @@
-use std::ffi::c_void;
-use std::string::String;
-
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
-    NumericLiteral { numeric_type: NumericLiteralType, value: i32 },
+    NumericLiteral { numeric_type: NumericLiteralType, value: String },
     StringLiteral { string_type: StringLiteralType, value: String },
+    DeclarationKeyword { keyword: DeclarationKeywords },
     Eof,
-    Let,
-    Bool,
-    LParen,
-    RParen,
-    LBracket,
-    RBracket,
-    LBrace,
-    RBrace,
-    Equal,
-    DoubleEqual,
-    NotEqual,
-    GreaterThan,
-    GreaterThanEqual,
-    LessThan,
-    LessThanEqual,
-    Identifier{identifier: String},
-    Semicolon,
-    Or,
-    And,
-    Not,
-    LineComment,
-    OpenComment,
-    CloseComment,
-    TypeSeparator,
-    AdditionSymbol,
-    SubtractionSymbol,
-    MultiplicationSymbol,
-    DivisionSymbol,
-    ArrowReturn,
-    SingleQuote,
-    DoubleQuote,
-    Pointer,
-    DereferencePointer,
-    Caster,
+    LParen, // (
+    RParen, // )
+    LBracket, // [
+    RBracket, // ]
+    LBrace, // {
+    RBrace, // }
+    Equal, // =
+    DoubleEqual, // ==
+    NotEqual, // !=
+    GreaterThan, // >
+    GreaterThanEqual, // >=
+    LessThan, // <
+    LessThanEqual, // <=
+    Identifier { identifier: String },
+    Semicolon, // ;
+    Or, // ||
+    And, // &&
+    Not, // !
+    LineComment, // //
+    OpenComment, // />
+    CloseComment, // </
+    TypeSeparator, // |
+    AdditionOp, // +
+    SubtractionOp, // -
+    MultiplicationOp, // *
+    DivisionOp, // /
+    ArrowReturn, // >>
+    SingleQuote, // '
+    DoubleQuote, // "
+    Pointer, // ^
+    Dereference, // &
+    Caster, // as
+    Colon, // :
+    Break, // break
+    Continue, // continue
+    Return, // return
+    If, // if
+    Else, // else
+    For, // for
+    Switch, // switch
+    Try, // try
+    Catch, // catch
+    Import, // import
+    Dot, // dot
+    This, // this
+    BackSlash, // \
+    BackSlashN, // \n
+    Ternary, // ?
+    Debug { debug: String }, //TODO Remove this when lexer is done
+}
+#[derive(Debug, PartialEq)]
+pub enum DeclarationKeywords {
     Class,
     Struct,
-    Colon,
-    Break,
-    Continue,
-    Return,
-    If,
-    Else,
-    For,
-    Switch,
-    Try,
-    Catch,
-    Import,
-    Dot,
-    This,
-    BackSlash,
-    Ternary,
-    Unknown{debug: String},
-    Uninitialized
-}
-enum DeclarationKeywords {
+    Let,
+    Bool,
     Int,
     Float,
     Double,
@@ -68,23 +67,35 @@ enum DeclarationKeywords {
     String,
     Char,
 }
-enum NumericLiteralType {
+#[derive(Debug, PartialEq)]
+pub enum NumericLiteralType {
     Int,
     Float,
     Double,
     Long,
 }
-enum StringLiteralType {
+#[derive(Debug, PartialEq)]
+pub enum StringLiteralType {
     String,
     Char,
 }
+#[derive(Debug, PartialEq)]
 pub enum LexerErrorType {
     InvalidCharacter,
 }
 
-pub(crate) struct Lexer {
-    pub tokens: Vec<TokenType>,
-}
+pub fn parse(input: String) -> Result<Vec<TokenType>, Vec<LexerErrorType>> {
+    let tokens: Vec<TokenType> = Vec::new();
+    let parse_errors: Vec<LexerErrorType> = Vec::new();
 
+
+
+    return if parse_errors.len() > 0 { Err(parse_errors) } else { Ok(Token) }
+}
 impl Lexer {
+    pub(crate) fn new(input: String) -> Lexer {
+        Lexer {
+            tokens: vec![], input
+        }
+    }
 }
