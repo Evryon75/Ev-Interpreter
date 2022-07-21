3c8684da7db4683f33b4b9e1b3d343c1895aa131
Evryon <fireden70@gmail.com>
2022-07-21 20:27:22 +0200

    Implemented string literals

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,6 @@
+use crate::lexer::TokenType::NumericLiteral;
 use std::alloc::handle_alloc_error;
 use std::ptr::addr_of;
-use crate::lexer::TokenType::NumericLiteral;
 
 pub fn tokenize(input: String) -> Result<Vec<TokenType>, Vec<LexerErrorType>> {
     let mut tokens: Vec<TokenType> = Vec::new();
@@ -11,12 +11,20 @@ pub fn tokenize(input: String) -> Result<Vec<TokenType>, Vec<LexerErrorType>> {
 
     let mut building_token: String = "".parse().unwrap();
     let mut cursor = 0;
-
     while cursor < raw_input_vec.len() {
         building_token.push(raw_input_vec[cursor]);
+        if building_token.as_str() == " " {
+            building_token = "".parse().unwrap();
+        }
 
-        let analysis_result= analyze_token( &building_token,
-if cursor < raw_input_vec.len() - 1 { raw_input_vec[cursor + 1] } else { ' ' });
+        let analysis_result = analyze_token(
+            &building_token,
+            if cursor < raw_input_vec.len() - 1 {
+                raw_input_vec[cursor + 1]
+            } else {
+                '⨂'
+            },
+        );
 
         if analysis_result.1 == LexerErrorType::None {
             if analysis_result.0 != TokenType::None {
@@ -28,11 +36,14 @@ if cursor < raw_input_vec.len() - 1 { raw_input_vec[cursor + 1] } else { ' ' });
         }
         cursor += 1;
     }
-    println!("Everything is working properly up to here");
-    if parse_errors.len() > 0 { Err(parse_errors) } else { Ok(tokens) }
+
+    if parse_errors.len() > 0 {
+        Err(parse_errors)
+    } else {
+        Ok(tokens)
+    }
 }
 fn analyze_token(token: &String, next_char: char) -> (TokenType, LexerErrorType) {
-
     let mut resulting_token: TokenType = TokenType::None;
     let mut error: LexerErrorType = LexerErrorType::None;
 
@@ -41,125 +52,179 @@ fn analyze_token(token: &String, next_char: char) -> (TokenType, LexerErrorType)
     //TODO Special cases like >=, ==, etc. Check the double ones first for efficiency
     //TODO Match that checks the rest
 
-    if resulting_token == TokenType::None && error == LexerErrorType::None { // Numeric literal
+    // Numeric literals
+    if resulting_token == TokenType::None
+        && error == LexerErrorType::None
+        && !token.starts_with('"')
+    {
         let mut dot = false;
         let mut valid_num = true;
-        token.trim().chars().for_each(|c|{
+        token.trim().chars().for_each(|c| {
             if !".0123456789".contains(c) {
                 valid_num = false;
             }
-            if !dot { if c == '.' { dot = true }
+            if !dot {
+                if c == '.' {
+                    dot = true
+                }
             } else {
-                if c == '.' { error = LexerErrorType::InvalidFloatingPoint; valid_num = false }
+                if c == '.' {
+                    error = LexerErrorType::InvalidFloatingPoint;
+                    valid_num = false
+                }
             }
-            if token.starts_with('.') { valid_num = false }
+            if token.starts_with('.') {
+                valid_num = false
+            }
         });
-        if !".0123456789".contains(next_char) && valid_num {
-            resulting_token = TokenType::NumericLiteral
-            { numeric_type:
-            if token.contains('.') {
-                let mut post_dot = false;
-                let mut decimals = 0;
-                token.chars().for_each(|c| {
-                    if post_dot {
-                        decimals += 1;
+        if !".0123456789".contains(next_char) && valid_num && token.len() != 0 {
+            resulting_token = NumericLiteral {
+                numeric_type: if token.contains('.') {
+                    let mut post_dot = false;
+                    let mut decimals = 0;
+                    token.chars().for_each(|c| {
+                        if post_dot {
+                            decimals += 1;
+                        }
+                        if c == '.' && !post_dot {
+                            post_dot = true
+                        }
+                    });
+                    if decimals < 8 {
+                        NumericLiteralType::Float
+                    } else {
+                        NumericLiteralType::Double
                     }
-                    if c == '.' && !post_dot { post_dot = true}
-                });
-                if decimals < 8 { NumericLiteralType::Float } else { NumericLiteralType::Double }
-            } else if token.len() < 17 { NumericLiteralType::Int }
-            else {
-                NumericLiteralType::Long }, value: token.trim().parse::<f64>().unwrap()
+                } else if token.len() < 17 {
+                    NumericLiteralType::Int
+                } else {
+                    NumericLiteralType::Long
+                },
+                value: token.trim().parse::<f64>().unwrap(),
             };
         }
     }
+    // String literals
     if resulting_token == TokenType::None && error == LexerErrorType::None {
-
+        // String literal
+        if token.starts_with('"') && token.ends_with('"') && token.len() > 1 {
+            resulting_token = TokenType::StringLiteral {
+                string_type: StringLiteralType::String,
+                value: token.replace("\"", "").to_string(),
+            };
+        } else if token.starts_with('"') && next_char == '⨂' {
+            error = LexerErrorType::StringLiteralDoesNotEnd;
+        }
+        if token.starts_with("'") && token.ends_with("'") && token.len() > 1 {
+            if token.len() < 4 {
+                resulting_token = TokenType::StringLiteral {
+                    string_type: StringLiteralType::Char,
+                    value: token.replace("'", "").to_string(),
+                };
+            } else {
+                error = LexerErrorType::CharIsTooLong
+            }
+        } else if token.starts_with("'") && next_char == '⨂' {
+            error = LexerErrorType::StringLiteralDoesNotEnd;
+        }
     }
-
+    // Identifier
+    if resulting_token == TokenType::None && error == LexerErrorType::None {
+            
+    }
     (resulting_token, error)
 }
 
-fn parser() {
-
-}
+fn parser() {}
 
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
-    NumericLiteral { numeric_type: NumericLiteralType, value: f64 },
-    StringLiteral { string_type: StringLiteralType, value: String },
-    DeclarationKeyword { keyword: DeclarationKeywords },
+    NumericLiteral {
+        numeric_type: NumericLiteralType,
+        value: f64,
+    },
+    StringLiteral {
+        string_type: StringLiteralType,
+        value: String,
+    },
+    Identifier {
+        identifier: String,
+    },
+    DeclarationKeyword {
+        keyword: DeclarationKeywords,
+    },
     Eof,
-    LParen, // (
-    RParen, // )
-    LBracket, // [
-    RBracket, // ]
-    LBrace, // {
-    RBrace, // }
-    Equal, // =
-    DoubleEqual, // ==
-    NotEqual, // !=
-    GreaterThan, // >
+    LParen,           // (
+    RParen,           // )
+    LBracket,         // [
+    RBracket,         // ]
+    LBrace,           // {
+    RBrace,           // }
+    Equal,            // =
+    DoubleEqual,      // ==
+    NotEqual,         // !=
+    GreaterThan,      // >
     GreaterThanEqual, // >=
-    LessThan, // <
-    LessThanEqual, // <=
-    Identifier { identifier: String },
-    Semicolon, // ;
-    Or, // ||
-    And, // &&
-    Not, // !
-    LineComment, // //
-    OpenComment, // />
-    CloseComment, // </
-    TypeSeparator, // |
-    AdditionOp, // +
-    SubtractionOp, // -
+    LessThan,         // <
+    LessThanEqual,    // <=
+    Semicolon,        // ;
+    Or,               // ||
+    And,              // &&
+    Not,              // !
+    LineComment,      // //
+    OpenComment,      // />
+    CloseComment,     // </
+    TypeSeparator,    // |
+    AdditionOp,       // +
+    SubtractionOp,    // -
     MultiplicationOp, // *
-    DivisionOp, // /
-    ArrowReturn, // >>
-    SingleQuote, // '
-    DoubleQuote, // "
-    Pointer, // ^
-    Dereference, // &
-    Caster, // as
-    Colon, // :
-    Break, // break
-    Continue, // continue
-    Return, // return
-    If, // if
-    Else, // else
-    For, // for
-    Switch, // switch
-    Try, // try
-    Catch, // catch
-    Import, // import
-    Dot, // dot
-    This, // this
-    BackSlash, // \
-    BackSlashN, // \n
-    Ternary, // ?
-    None, // No token found, gets removed later
-    Debug { debug: String }, //TODO Remove this when lexer is done
+    DivisionOp,       // /
+    ArrowReturn,      // >>
+    SingleQuote,      // '
+    DoubleQuote,      // "
+    Pointer,          // ^
+    Dereference,      // &
+    Caster,           // as
+    Colon,            // :
+    Break,            // break
+    Continue,         // continue
+    Return,           // return
+    If,               // if
+    Else,             // else
+    For,              // for
+    Switch,           // switch
+    Try,              // try
+    Catch,            // catch
+    Import,           // import
+    Dot,              // dot
+    This,             // this
+    BackSlash,        // \
+    BackSlashN,       // \n
+    Ternary,          // ?
+    None,             // No token found, gets removed later
+    Debug {
+        debug: String,
+    }, //TODO Remove this when lexer is done
 }
 #[derive(Debug, PartialEq)]
 pub enum DeclarationKeywords {
-    Class,
-    Struct,
-    Let,
-    Bool,
-    Int,
-    Float,
-    Double,
-    Long,
-    String,
-    Char,
+    Class,  // class
+    Struct, // struct
+    Let,    // let
+    Bool,   // bool
+    Int,    // int
+    Float,  // float
+    Double, // double
+    Long,   // long
+    String, // string
+    Char,   // char
 }
 #[derive(Debug, PartialEq)]
 pub enum NumericLiteralType {
-    Int, // i32
-    Float, // f32
+    Int,    // i32
+    Float,  // f32
     Double, // f64
-    Long, // i64
+    Long,   // i64
 }
 #[derive(Debug, PartialEq)]
 pub enum StringLiteralType {
@@ -170,5 +235,7 @@ pub enum StringLiteralType {
 pub enum LexerErrorType {
     InvalidCharacter,
     InvalidFloatingPoint,
-    None
+    StringLiteralDoesNotEnd,
+    CharIsTooLong,
+    None,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use crate::lexer::tokenize;
+use std::fs::{read, File};
+use std::io::Read;
 
 mod ast;
 mod lexer;
 
 fn main() {
-    let test_input: String = "3 5".parse().unwrap();
-    let tokenized_input = tokenize(test_input);
+    let mut file_input = File::open("hello world.ev").expect("Something went wrong");
+    let mut reading_input: String = "".to_string();
+
+    file_input
+        .read_to_string(&mut reading_input)
+        .expect("Something went wrong");
+
+    let tokenized_input = tokenize(reading_input);
     for i in tokenized_input.unwrap() {
         println!("\n {:?}", i);
     }
