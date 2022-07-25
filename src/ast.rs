abd31707b83b2fe0cad519a10ffcc8551e3df16b
Evryon <fireden70@gmail.com>
2022-07-25 22:32:42 +0200

    Excluded comments from lexing

--- src/ast.rs
+++ src/ast.rs
@@ -4,16 +4,36 @@ pub(crate) fn parse_tokens(tokens: Vec<TokenType>) {
     for i in tokens {
         println!("\n {:?}", i);
     }
+
+    let ast = AbstractSyntaxTree { program: vec![] };
+    for i in ast.program {
+        println!("{:?}", i);
+    }
 }
 
 struct AbstractSyntaxTree {
-    program: Vec<Node>
+    pub program: Vec<Node>,
 }
+impl AbstractSyntaxTree {}
 
+#[derive(Debug, PartialEq)]
+enum Operator {
+    Plus,
+    Minus,
+}
+#[derive(Debug, PartialEq)]
 enum Node {
-    NumericLiteral{number_type: NumericLiteralType, value: i64},
-    Subtraction{left: Node, right: Node},
-    Addition{left: Node, right: Node},
-    Multiplication{left: Node, right: Node},
-    Division{left: Node, right: Node},
+    NumericLiteral {
+        numeric_type: NumericLiteralType,
+        value: f64,
+    },
+    UnaryExpression {
+        op: Operator,
+        child: Box<Node>,
+    },
+    BinaryExpression {
+        op: Operator,
+        lhs: Box<Node>,
+        rhs: Box<Node>,
+    },
 }
--- src/lexer.rs
+++ src/lexer.rs
@@ -26,12 +26,13 @@ pub fn tokenize(input: String) -> Result<Vec<TokenType>, Vec<LexerErrorType>> {
                 raw_input_vec[cursor + 1]
             } else {
                 EOF_SYMBOL
-            },
-        );
+            });
 
         if analysis_result.1 == LexerErrorType::None {
-            if analysis_result.0 != TokenType::None {
+            if analysis_result.0 != TokenType::None && analysis_result.0 != TokenType::LineComment{
                 tokens.push(analysis_result.0);
+                building_token = "".parse().unwrap();
+            } else if analysis_result.0 == TokenType::LineComment {
                 building_token = "".parse().unwrap();
             }
         } else {
@@ -55,166 +56,170 @@ fn analyze_token(token: &String, next_char: char) -> (TokenType, LexerErrorType)
     let mut resulting_token: TokenType = None;
     let mut error: LexerErrorType = LexerErrorType::None;
 
-    //Simple tokens
-    resulting_token = match token.as_str() {
-        "int" => DeclarationKeyword { keyword: Int },
-        "long" => DeclarationKeyword { keyword: Long },
-        "float" => DeclarationKeyword { keyword: Float },
-        "double" => DeclarationKeyword { keyword: Double },
-        "string" => DeclarationKeyword { keyword: String },
-        "char" => DeclarationKeyword { keyword: Char },
-        "let" => DeclarationKeyword { keyword: Let },
-        "bool" => DeclarationKeyword { keyword: Bool },
-        "struct" => DeclarationKeyword { keyword: Struct },
-        "class" => DeclarationKeyword { keyword: Class },
-        "(" => LParen,
-        ")" => RParen,
-        "[" => LBracket,
-        "]" => RBracket,
-        "{" => LBrace,
-        "}" => RBrace,
-        "==" => DoubleEqual,
-        ">=" => GreaterThanEqual,
-        "<=" => LessThanEqual,
-        ";" => Semicolon,
-        "||" => Or,
-        "&&" => And,
-        "!" => Not,
-        "//" => LineComment,
-        "+" => AdditionOp,
-        "-" => SubtractionOp,
-        "*" => MultiplicationOp,
-        ">>" => ArrowReturn,
-        "#" => Pointer,
-        "@" => Dereference,
-        ":" => Colon,
-        "break" => Break,
-        "continue" => Continue,
-        "return" => Return,
-        "if" => If,
-        "else" => Else,
-        "for" => For,
-        "try" => Try,
-        "catch" => Catch,
-        "import" => Import,
-        "." => Dot,
-        "this" => This,
-        "?" => Ternary,
-        "," => Comma,
-        "true" => BooleanLiteral{ value: true },
-        "false" => BooleanLiteral{ value: false },
-        &_ => None,
-    };
+    if !token.trim().starts_with("//") {
+        //Simple tokens
+        resulting_token = match token.as_str() {
+            "int" => DeclarationKeyword { keyword: Int },
+            "long" => DeclarationKeyword { keyword: Long },
+            "float" => DeclarationKeyword { keyword: Float },
+            "double" => DeclarationKeyword { keyword: Double },
+            "string" => DeclarationKeyword { keyword: String },
+            "char" => DeclarationKeyword { keyword: Char },
+            "let" => DeclarationKeyword { keyword: Let },
+            "bool" => DeclarationKeyword { keyword: Bool },
+            "struct" => DeclarationKeyword { keyword: Struct },
+            "class" => DeclarationKeyword { keyword: Class },
+            "(" => LParen,
+            ")" => RParen,
+            "[" => LBracket,
+            "]" => RBracket,
+            "{" => LBrace,
+            "}" => RBrace,
+            "==" => DoubleEqual,
+            ">=" => GreaterThanEqual,
+            "<=" => LessThanEqual,
+            ";" => Semicolon,
+            "||" => Or,
+            "&&" => And,
+            "!" => Not,
+            "+" => AdditionOp,
+            "-" => SubtractionOp,
+            "*" => MultiplicationOp,
+            ">>" => ArrowReturn,
+            "#" => Pointer,
+            "@" => Dereference,
+            ":" => Colon,
+            "break" => Break,
+            "continue" => Continue,
+            "return" => Return,
+            "if" => If,
+            "else" => Else,
+            "for" => For,
+            "try" => Try,
+            "catch" => Catch,
+            "import" => Import,
+            "." => Dot,
+            "this" => This,
+            "?" => Ternary,
+            "," => Comma,
+            "true" => BooleanLiteral { value: true },
+            "false" => BooleanLiteral { value: false },
+            &_ => None,
+        };
 
-    // Could probably optimise by doing resulting_token = if token * and next token !* {one} else {two}
-    // but im unsure about the behaviour it could generate
-    if token == "/" && next_char != '/' {
-        resulting_token = DivisionOp;
-    }
-    if token == "|" && next_char != '|' {
-        resulting_token = TypeSeparator;
-    }
-    if token == "=" && next_char != '=' {
-        resulting_token = Equal;
-    }
-    if token == ">" && next_char != '=' {
-        resulting_token = GreaterThan;
-    }
-    if token == "<" && next_char != '=' {
-        resulting_token = LessThan;
-    }
-    if token == "as" && next_char == ' ' {
-        resulting_token = Caster;
-    }
+        // Could probably optimise by doing resulting_token = if token * and next token !* {one} else {two}
+        // but im unsure about the behaviour it could generate
+        if token == "/" && next_char != '/' {
+            resulting_token = DivisionOp;
+        }
+        if token == "|" && next_char != '|' {
+            resulting_token = TypeSeparator;
+        }
+        if token == "=" && next_char != '=' {
+            resulting_token = Equal;
+        }
+        if token == ">" && next_char != '=' {
+            resulting_token = GreaterThan;
+        }
+        if token == "<" && next_char != '=' {
+            resulting_token = LessThan;
+        }
+        if token == "as" && next_char == ' ' {
+            resulting_token = Caster;
+        }
 
-    // Numeric literals
-    if resulting_token == None && error == LexerErrorType::None {
-        let mut dot = false;
-        let mut valid_num = !token.is_empty(); // If its empty, default to false
-        token.trim().chars().for_each(|c| {
-            if !".0123456789".contains(c) {
-                valid_num = false;
-            }
-            if !dot {
-                if c == '.' {
-                    dot = true
+        // Numeric literals
+        if resulting_token == None && error == LexerErrorType::None {
+            let mut dot = false;
+            let mut valid_num = !token.is_empty(); // If its empty, default to false
+            token.trim().chars().for_each(|c| {
+                if !".0123456789".contains(c) {
+                    valid_num = false;
                 }
-            } else {
-                if c == '.' {
-                    error = LexerErrorType::InvalidFloatingPoint;
+                if !dot {
+                    if c == '.' {
+                        dot = true
+                    }
+                } else {
+                    if c == '.' {
+                        error = LexerErrorType::InvalidFloatingPoint;
+                        valid_num = false
+                    }
+                }
+                if token.starts_with('.') {
                     valid_num = false
                 }
-            }
-            if token.starts_with('.') {
-                valid_num = false
-            }
-        });
+            });
 
-        if !".0123456789".contains(next_char) && valid_num && !token.is_empty() {
-            resulting_token = NumericLiteral {
-                numeric_type: if token.contains('.') {
-                    let mut post_dot = false;
-                    let mut decimals = 0;
-                    token.chars().for_each(|c| {
-                        if post_dot {
-                            decimals += 1;
+            if !".0123456789".contains(next_char) && valid_num && !token.is_empty() {
+                resulting_token = NumericLiteral {
+                    numeric_type: if token.contains('.') {
+                        let mut post_dot = false;
+                        let mut decimals = 0;
+                        token.chars().for_each(|c| {
+                            if post_dot {
+                                decimals += 1;
+                            }
+                            if c == '.' && !post_dot {
+                                post_dot = true
+                            }
+                        });
+                        if decimals < 8 {
+                            NumericLiteralType::Float
+                        } else {
+                            NumericLiteralType::Double
                         }
-                        if c == '.' && !post_dot {
-                            post_dot = true
-                        }
-                    });
-                    if decimals < 8 {
-                        NumericLiteralType::Float
+                    } else if token.len() < 17 {
+                        NumericLiteralType::Int
                     } else {
-                        NumericLiteralType::Double
-                    }
-                } else if token.len() < 17 {
-                    NumericLiteralType::Int
-                } else {
-                    NumericLiteralType::Long
-                },
-                value: token.trim().parse::<f64>().unwrap(),
-            };
-        }
-    }
-    // String literals
-    if resulting_token == None && error == LexerErrorType::None {
-        // String literal
-        if token.starts_with('"') && token.ends_with('"') && token.len() > 1 {
-            resulting_token = StringLiteral {
-                string_type: StringLiteralType::String,
-                value: token.replace("\"", "").to_string(),
-            };
-        } else if token.starts_with('"') && next_char == EOF_SYMBOL {
-            error = LexerErrorType::StringLiteralDoesNotEnd;
-        }
-        if token.starts_with("'") && token.ends_with("'") && token.len() > 1 {
-            if token.len() < 4 {
-                resulting_token = StringLiteral {
-                    string_type: StringLiteralType::Char,
-                    value: token.replace("'", "").to_string(),
+                        NumericLiteralType::Long
+                    },
+                    value: token.trim().parse::<f64>().unwrap(),
                 };
-            } else {
-                error = LexerErrorType::CharIsTooLong
             }
-        } else if token.starts_with("'") && next_char == EOF_SYMBOL {
-            error = LexerErrorType::StringLiteralDoesNotEnd;
         }
-    }
-    // Identifier
-    if resulting_token == None && error == LexerErrorType::None {
-        if (" .?^'{[()]}+-/*!|;=\"".contains(next_char) || next_char == EOF_SYMBOL)
-            && valid_identifier(token)
-            && !token.starts_with('\"')
-            && !token.is_empty()
-            && token.is_ascii()
-        {
-            resulting_token = Identifier {
-                identifier: token.to_string(),
+        // String literals
+        if resulting_token == None && error == LexerErrorType::None {
+            // String literal
+            if token.starts_with('"') && token.ends_with('"') && token.len() > 1 {
+                resulting_token = StringLiteral {
+                    string_type: StringLiteralType::String,
+                    value: token.replace("\"", "").to_string(),
+                };
+            } else if token.starts_with('"') && next_char == EOF_SYMBOL {
+                error = LexerErrorType::StringLiteralDoesNotEnd;
             }
-        } else if !token.is_ascii() {
-            error = LexerErrorType::NonAsciiCharactersInIdentifier
+            if token.starts_with("'") && token.ends_with("'") && token.len() > 1 {
+                if token.len() < 4 {
+                    resulting_token = StringLiteral {
+                        string_type: StringLiteralType::Char,
+                        value: token.replace("'", "").to_string(),
+                    };
+                } else {
+                    error = LexerErrorType::CharIsTooLong
+                }
+            } else if token.starts_with("'") && next_char == EOF_SYMBOL {
+                error = LexerErrorType::StringLiteralDoesNotEnd;
+            }
         }
+        // Identifier
+        if resulting_token == None && error == LexerErrorType::None {
+            if (" .?^'{[()]}+-/*!|;=\"".contains(next_char) || next_char == EOF_SYMBOL)
+                && valid_identifier(token)
+                && !token.starts_with('\"')
+                && !token.is_empty()
+                && token.is_ascii()
+            {
+                resulting_token = Identifier {
+                    identifier: token.to_string(),
+                }
+            } else if !token.is_ascii() {
+                error = LexerErrorType::NonAsciiCharactersInIdentifier
+            }
+        }
+    } else if token.contains('\n') {
+        resulting_token = LineComment;
+        println!("{}", token)
     }
 
     if next_char == EOF_SYMBOL {
@@ -244,7 +249,9 @@ pub enum TokenType {
     DeclarationKeyword {
         keyword: DeclarationKeywords,
     },
-    BooleanLiteral {value: bool},
+    BooleanLiteral {
+        value: bool,
+    },
     LParen,           // (
     RParen,           // )
     LBracket,         // [
